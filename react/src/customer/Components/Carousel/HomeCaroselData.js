2f5022a27e03b7656616a6d4a05f4453b8d0fbf1
SpYRoNy <112245741+SpYRoNy@users.noreply.github.com>
2024-12-19 14:52:41 +0530

    Initial commit

---
+++ react/src/customer/Components/Carousel/HomeCaroselData.js
@@ -0,0 +1,12 @@
+export const homeCarouselData=[
+    {
+        image:"https://www.cdac.in/index.aspx?id=img_Bengaluru-cb",
+    },
+    {
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYOcUeG4ZBfcy1cfP0lhoj3H_66LRxuRoFew&s",
+    },
+    {
+        image:"https://c-huk.cdacb.in/assets/img/Cdac_img.jpg",
+    }
+]
+
